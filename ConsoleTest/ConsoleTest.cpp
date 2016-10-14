789a7cb68c74aee6e045bc74620d7fb7e05a09ac
yinwenjie <eternal.ywj@gmail.com>
2016-10-14 12:25:28 +0800

    清理ConsoleTest

--- ConsoleTest/ConsoleTest.cpp
+++ ConsoleTest/ConsoleTest.cpp
@@ -6,29 +6,8 @@
 #include <iostream>
 using namespace std;
 
-ofstream timeline;
-int videoIdx = 0, audioIdx = 0;
-
 void edit_tag(CFlvTag *tag)
 {
-	timeline << "-----------------------------" << endl;
-	if (tag->Get_tag_type() == TAG_TYPE_VIDEO)
-	{
-		timeline << "Video Idx: " << videoIdx++ << endl;
-		timeline << "Time Stamp: " << to_string(tag->Get_tag_timestamp_ext())<< endl;
-	}
-	else if (tag->Get_tag_type() == TAG_TYPE_AUDIO)
-	{
-		if (audioIdx < 1)
-		{
-			audioIdx++;
-			return;
-		}
-		double actualTime = (audioIdx - 1) * 1024000.0 / 44100.0;
-		timeline << "Audio Idx: " << audioIdx++ << endl;
-		timeline << "Time Stamp:  " << to_string(tag->Get_tag_timestamp_ext()) << endl;
-		timeline << "Actual Time: " << to_string(actualTime) << endl;
-	}
 	return;
 }
 
@@ -42,9 +21,6 @@ int main(int argc, char **argv)
 		cout << argv[1] <<" : " << errorHints[-err] << endl;
 		return -1;
 	}
-	
-	timeline.open("timeline.txt");
-	timeline << argv[1] << endl;
 
 	CFlvWriter writer(argv[2], &parser);
 	if (writer.Init(true, true))
@@ -55,9 +31,7 @@ int main(int argc, char **argv)
 //	writer.Clone_FLV_with_video();
 //	writer.Create_FLV_with_edited_tag(edit_tag);
 	writer.Extract_tags_with_range(10, 1778);
-
-	timeline.close();
-
+	
     return 0;
 }
 
