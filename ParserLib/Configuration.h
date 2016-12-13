92b8e2ae6dbadb4b28d49028dbe919a8b13f256b
yinwenjie <eternal.ywj@gmail.com>
2016-12-13 11:36:48 +0800

    输出音频的log

--- ParserLib/Configuration.h
+++ ParserLib/Configuration.h
@@ -13,7 +13,7 @@
 
 #define DUMP_AUDIO_TAG_INFO_ENABLED_LOG 1				//在日志文件中输出音频Tag详细信息
 
-#define DUMP_ONLY_VIDEO_TAG_INFO 1						//只输出视频Tag的信息
+#define DUMP_ONLY_VIDEO_TAG_INFO 0						//只输出视频Tag的信息
 
 #define DUMP_SCRIPT_INFO_ENABLED_LOG 1					//在日志文件中输出视频script信息
 #define DUMP_SCRIPT_INFO_ENABLED_CONSOLE 1				//控制台窗口输出Script Tag详细信息
